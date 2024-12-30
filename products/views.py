ef6063bbaea01d6142b7917280fd6b390e71388c
nikachaduneli <chaduneli@hmg.software>
2024-12-30 14:36:52 +0400

    first commit

---
+++ products/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from .models import Product
+
+
+def home(request):
+    products = Product.objects.all()
+
+    return render(request, 'home.html', {'products': products})
+
+
+def product_detail(request):
+    return render(request, 'product_detail.html')
